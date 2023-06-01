8c9aaa94a7afe43206782d006f13e4fd646dec27
tdphatstudy <tdphat.study@gmail.com>
2023-06-01 22:23:47 +0700

    add 404 page

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,12 @@
+import '../assets/css/page/error404.css';
+const Error404 = () => {
+    return (
+    <div className="error-not-found-wrapper">
+        <span>404</span>
+        <div>OOPS! Page Not Found</div>
+        <p>Sorry, the page you are looking for does not exist. Please return to the homepage.</p>
+    </div>
+    )
+}
+
+export default Error404;
---
+++ src/pages/ShopingCart.jsx
@@ -0,0 +1,8 @@
+const ShoppingCart = () => {
+    return 
+    (
+    <div></div>
+    )
+}
+
+export default ShoppingCart;
